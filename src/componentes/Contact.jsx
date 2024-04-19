498f383b4ec2285d94b49a3d02c8270fe7e64fe0
Danuz05 <jhonanduz95@gmail.com>
2024-04-19 00:16:27 -0400

    contact

--- src/componentes/Contact.jsx
+++ src/componentes/Contact.jsx
@@ -8,7 +8,7 @@ export const Contact = () => {
       <div className='contact'>
         <footer className='footer'>
           <h2 className='h2 center'>{Spanish ? "Contáctame" : "Contact me"}</h2>
-          <p>{Spanish ? "Teléfono" : "Phone number"}: +58-412-1818892</p>
+          <p>{Spanish ? "Teléfono" : "Phone number"}: <strong>+58-412-1818892</strong></p>
           <article className='contact-article'>
             <a className='contact-a hover-contact' href="mailto:jhonanduz95@gmail.com"><i class="fa-solid fa-envelope"></i>
             </a>
--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -8,7 +8,6 @@ import { FaSass } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
 import { SiAstro } from "react-icons/si";
 
-
 export const Main = () => {
 
     const Spanish = useUserSpanish()
@@ -16,23 +15,18 @@ export const Main = () => {
         <div className='column'>
             <section className='me' id='about-me'>
                 <h2 className='h2'>{Spanish ? "Sobre mi" : "About me"}</h2>
-
                 <p>{Spanish ? `Mi nombre es Jhon Anduz, tengo 20 años, soy de Venezuela, mi objetivo es conseguir mi primer trabajo como desarrollador web front end` :
                     `My name is Jhon Anduz, I am 20 years old, I am from Venezuela, my goal is to get my first job as a front end web developer.`}
                 </p>
-
                 <article className='buttons'>
-                    <a className='buttons-social' href='#'>github <i class="fa-brands fa-github"></i></a>
+                    <a className='buttons-social' href='https://github.com/Danuz05/jhonAnduz'>github <i class="fa-brands fa-github"></i></a>
                     <a className='buttons-social' href='https://www.linkedin.com/in/jhon-anduz-buriel-b96109280' target='_blank'>LinkedIn <i class="fa-brands fa-linkedin"></i></a>
                 </article>
             </section>
-
             <section className='skills'>
                 <h2 className='h2 h2-skills'>Skills</h2>
                 <section className='skills-section'>
-                <DiHtml5 className='html5' />
-
-
+                    <DiHtml5 className='html5' />
                     <FaCss3Alt className='css3' />
                     <RiJavascriptFill className='js' />
                     <FaSass className='sass' />
