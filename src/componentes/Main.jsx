5b0352e7277571293002f85858a9b1d5371a75f2
Danuz05 <jhonanduz95@gmail.com>
2024-04-22 14:33:39 -0400

    githun

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -26,7 +26,7 @@ export const AboutMe = () => {
                 `My name is Jhon Anduz, I am 20 years old, I am from Venezuela, my goal is to get my first job as a front end web developer.`}
             </p>
             <article className='buttons'>
-                <a className='buttons-social' href='https://github.com/Danuz05/jhonAnduz' target='_blank'>github <i className="fa-brands fa-github"></i></a>
+                <a className='buttons-social' href='https://github.com/Danuz05' target='_blank'>github <i className="fa-brands fa-github"></i></a>
                 <a className='buttons-social' href='https://www.linkedin.com/in/jhon-anduz-buriel-b96109280' target='_blank'>LinkedIn <i className="fa-brands fa-linkedin"></i></a>
             </article>
         </section>
