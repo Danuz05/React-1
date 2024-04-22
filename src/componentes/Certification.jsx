c4e4104cb4bd74f87115fe94c0012c0b3df90dbd
Danuz05 <jhonanduz95@gmail.com>
2024-04-22 14:30:52 -0400

    finish4

--- src/componentes/Certification.jsx
+++ src/componentes/Certification.jsx
@@ -10,16 +10,30 @@ export const Certification = () => {
             <div>
                 <h2 className='h2 center'>{Spanish ? "Certificaciones" : "Certifications"}</h2>
                 <section className='certification-section'>
-                    <article className='certification-article'>
-                        <a href='https://www.freecodecamp.org/espanol/certification/jhon-anduz/responsive-web-design' target='_blank'>
-                            <img className='certification-img' alt='Responsive wev design freeCodeCamp camp 300 horas' src={freecodecamp} ></img></a>
-                    </article>
-                    <article className='certification-article'>
-                        <a href='https://production-egg-certificates.s3.amazonaws.com/JhonAnduz/ProgramacionWebdesdeCero/thumbnail_9228c779b2634d47cad120d0e250b8ace3d9fd5225551a81f01c75beef01176f.jpeg' target='_blank'>
-                            <img className='certification-img' src={egg} alt="curso de egg 32 horas" ></img></a>
-                    </article>
+                    <Freecode />
+                    <Egg />
                 </section>
             </div>
         </div>
+    )
+}
+
+export const Freecode = () => {
+    return (
+        <article className='certification-article'>
+            <a href='https://www.freecodecamp.org/espanol/certification/jhon-anduz/responsive-web-design' target='_blank'>
+                <img className='certification-img' alt='Responsive wev design freeCodeCamp camp 300 horas' src={freecodecamp} >
+                </img>
+            </a>
+        </article>
+    )
+}
+
+export const Egg = () => {
+    return (
+        <article className='certification-article'>
+            <a href='https://production-egg-certificates.s3.amazonaws.com/JhonAnduz/ProgramacionWebdesdeCero/thumbnail_9228c779b2634d47cad120d0e250b8ace3d9fd5225551a81f01c75beef01176f.jpeg' target='_blank'>
+                <img className='certification-img' src={egg} alt="curso de egg 32 horas" ></img></a>
+        </article>
     )
 }
--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,5 +1,5 @@
 import { useUserSpanish } from '../Userprovider'
-import React, { useState } from 'react'
+import React from 'react'
 import { BiLogoTypescript } from "react-icons/bi";
 import { DiHtml5 } from "react-icons/di";
 import { FaCss3Alt } from "react-icons/fa";
@@ -9,32 +9,43 @@ import { FaReact } from "react-icons/fa";
 import { SiAstro } from "react-icons/si";
 
 export const Main = () => {
-
-    const Spanish = useUserSpanish()
     return (
         <div className='column'>
-            <section className='me' id='about-me'>
-                <h2 className='h2'>{Spanish ? "Sobre mi" : "About me"}</h2>
-                <p>{Spanish ? `Mi nombre es Jhon Anduz, tengo 20 años, soy de Venezuela, mi objetivo es conseguir mi primer trabajo como desarrollador web front end` :
-                    `My name is Jhon Anduz, I am 20 years old, I am from Venezuela, my goal is to get my first job as a front end web developer.`}
-                </p>
-                <article className='buttons'>
-                    <a className='buttons-social' href='https://github.com/Danuz05/jhonAnduz'>github <i className="fa-brands fa-github"></i></a>
-                    <a className='buttons-social' href='https://www.linkedin.com/in/jhon-anduz-buriel-b96109280' target='_blank'>LinkedIn <i className="fa-brands fa-linkedin"></i></a>
-                </article>
-            </section>
-            <section className='skills'>
-                <h2 className='h2 h2-skills'>Skills</h2>
-                <section className='skills-section'>
-                    <DiHtml5 className='html5' />
-                    <FaCss3Alt className='css3' />
-                    <RiJavascriptFill className='js' />
-                    <FaSass className='sass' />
-                    <BiLogoTypescript className='type' />
-                    <FaReact className='react' />
-                    <SiAstro className='astro' />
-                </section>
-            </section>
+            <AboutMe />
+            <Skills />
         </div>
+    )
+}
+
+export const AboutMe = () => {
+    const Spanish = useUserSpanish()
+    return (
+        <section className='me' id='about-me'>
+            <h2 className='h2'>{Spanish ? "Sobre mi" : "About me"}</h2>
+            <p>{Spanish ? `Mi nombre es Jhon Anduz, tengo 20 años, soy de Venezuela, mi objetivo es conseguir mi primer trabajo como desarrollador web front end` :
+                `My name is Jhon Anduz, I am 20 years old, I am from Venezuela, my goal is to get my first job as a front end web developer.`}
+            </p>
+            <article className='buttons'>
+                <a className='buttons-social' href='https://github.com/Danuz05/jhonAnduz'>github <i className="fa-brands fa-github"></i></a>
+                <a className='buttons-social' href='https://www.linkedin.com/in/jhon-anduz-buriel-b96109280' target='_blank'>LinkedIn <i className="fa-brands fa-linkedin"></i></a>
+            </article>
+        </section>
+    )
+}
+
+export const Skills = () => {
+    return (
+        <section className='skills'>
+            <h2 className='h2 h2-skills'>Skills</h2>
+            <section className='skills-section'>
+                <DiHtml5 className='html5' />
+                <FaCss3Alt className='css3' />
+                <RiJavascriptFill className='js' />
+                <FaSass className='sass' />
+                <BiLogoTypescript className='type' />
+                <FaReact className='react' />
+                <SiAstro className='astro' />
+            </section>
+        </section>
     )
 }
