2fba497464ac5a541a1f3c342bc7d328ef0fa5db
Danuz05 <jhonanduz95@gmail.com>
2024-04-24 23:27:03 -0400

    jjj

--- src/componentes/Contact.jsx
+++ src/componentes/Contact.jsx
@@ -8,15 +8,13 @@ export const Contact = () => {
       <div className='contact'>
         <footer className='footer'>
           <h2 className='h2 center'>{Spanish ? "Contáctame" : "Contact me"}</h2>
-         
+          <p>{Spanish?"teléfono":"Phone number"}: <strong>+58-412-1818892</strong></p>
           <article className='contact-article'>
-        
             <li className='email'>
               <p className='email-p'>jhonanduz95@gmail.com</p>
               <a className='contact-a hover-contact' href="mailto:jhonanduz95@gmail.com"><i className="fa-solid fa-envelope"></i>
               </a>
             </li>
-
             <a className='contact-a' href="https://www.linkedin.com/in/jhon-anduz-buriel-b96109280" target='_blank'>
               <i className="fa-brands fa-linkedin"></i>
             </a>
--- src/componentes/Projects.jsx
+++ src/componentes/Projects.jsx
@@ -8,23 +8,30 @@ export const Projects = () => {
             <section className='project-section' >
                 <h2 className='h2 center'>{Spanish ? "Proyectos" : "Projects"}</h2>
                 <article className='project-column'>
-                    <article className='project'>
-                        <h2 className='h2'>{Spanish ? "Calculadora" : "Calculator"}</h2>
-                        <a href='https://danuz05.github.io/calculadora2.github.io/' target='_blank'>
-                            <img alt='imagen de una calculadora gris con un fondo azul' src='https://github.com/Danuz05/jhon-anduz.github.io/blob/main/calculadora-2.png?raw=true'></img>
-                        </a>
-                        <p className='margin-project'>{Spanish ? `Calculadora que permite sumar, restar, dividir y multiplicar,
-                         inspirada en la calculadora de un videojuego llamado Dave the Diver.` : `Calculator that allows you to add,
-                          subtract, divide and multiply, inspired by the calculator in a video game called Dave the Diver.`}
-                        </p>
-                        <section className='skills-flex'>
-                            <li className=' buttons-descripcion'>HTML </li>
-                            <li className=' buttons-descripcion'>CSS </li>
-                            <li className=' buttons-descripcion'>javaScript </li>
-                        </section>
-                    </article>
+                    <Calculator />
                 </article>
             </section>
         </div>
+    )
+}
+
+export const Calculator = () => {
+    const Spanish = useUserSpanish()
+    return (
+        <article className='project'>
+            <h2 className='h2'>{Spanish ? "Calculadora" : "Calculator"}</h2>
+            <a href='https://danuz05.github.io/calculadora2.github.io/' target='_blank'>
+                <img alt='imagen de una calculadora gris con un fondo azul' src='https://github.com/Danuz05/jhon-anduz.github.io/blob/main/calculadora-2.png?raw=true'></img>
+            </a>
+            <p className='margin-project'>{Spanish ? `Calculadora que permite sumar, restar, dividir y multiplicar,
+                         inspirada en la calculadora de un videojuego llamado Dave the Diver.` : `Calculator that allows you to add,
+                          subtract, divide and multiply, inspired by the calculator in a video game called Dave the Diver.`}
+            </p>
+            <section className='skills-flex'>
+                <li className=' buttons-descripcion'>HTML </li>
+                <li className=' buttons-descripcion'>CSS </li>
+                <li className=' buttons-descripcion'>javaScript </li>
+            </section>
+        </article>
     )
 }
